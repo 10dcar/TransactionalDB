ed36f1927c7315fcd97d6d23e363284b6d5f3019
enatech_home <daniel.carbunescu@gmail.com>
2025-09-04 21:14:50 +0200

    fast search

--- src/main/java/org/example/TransactionalDB.java
+++ src/main/java/org/example/TransactionalDB.java
@@ -2,28 +2,33 @@ package org.example;
 
 import java.util.*;
 
-public class TransactionalDB {
+interface DB {
+    public boolean begin();
+    public boolean commit();
+    public boolean rollback();
+    public String get(String key);
+    public boolean set(String key, String value);
+}
+
+public class TransactionalDB implements DB {
     HashMap<String, String> db = new HashMap<>();
     Stack<HashMap<String, String>> operationsStack = new Stack<>();
+    HashMap<String, String> fastSearch = new HashMap<>();
 
     //Begins a transaction
     public boolean begin(){
-        this.operationsStack.add(new HashMap<>());
+        operationsStack.add(new HashMap<>());
 
         return true;
     }
     //Commits everything that happened after the begin
     public boolean commit(){
-        if(!this.operationsStack.isEmpty()) {
-            HashMap<String, String> keyValue = this.operationsStack.pop();
+        if(!operationsStack.isEmpty()) {
+            HashMap<String, String> keyValue = operationsStack.pop();
             if(keyValue != null) {
                 for (Map.Entry<String, String> entry : keyValue.entrySet()) {
-                    //set() repeated here
-                    if(this.operationsStack.lastElement() != null) {
-                        this.operationsStack.lastElement().put(entry.getKey(), entry.getValue());
-                    } else {
-                        db.put(entry.getKey(), entry.getValue());
-                    }
+                    //Local class set() method
+                    this.set(entry.getKey(), entry.getValue());
                 }
                 return true;
             }
@@ -33,7 +38,25 @@ public class TransactionalDB {
     //Removes everything after the begin
     public boolean rollback(){
         if(!operationsStack.isEmpty()) {
-            this.operationsStack.pop();
+            HashMap<String, String> keyValue = this.operationsStack.pop();
+            if(keyValue != null) {
+                for (Map.Entry<String, String> entry : keyValue.entrySet()) {
+                    fastSearch.remove(entry.getKey());
+                    //Search trough stack and db for the value interested
+                    boolean found = false;
+                    ListIterator<HashMap<String, String>> iterator = operationsStack.listIterator(operationsStack.size());
+                    while (iterator.hasPrevious()) {
+                        HashMap<String, String> entry_src = iterator.previous();
+                        if(entry_src != null && entry_src.containsKey(entry.getKey())) {
+                            found = true;
+                            fastSearch.put(entry.getKey(), entry_src.get(entry.getKey()));
+                        }
+                    }
+                    if(!found) {
+                        fastSearch.put(entry.getKey(), db.get(entry.getKey()));
+                    }
+                }
+            }
             return true;
         }
         return false;
@@ -41,22 +64,15 @@ public class TransactionalDB {
     //Gets a value from the database. It can happen during a transaction or outside a transaction.
     public String get(String key){
         String value = "";
-        ListIterator<HashMap<String, String>> iterator = this.operationsStack.listIterator(this.operationsStack.size());
-        while (iterator.hasPrevious()) {
-            HashMap<String, String> entry = iterator.previous();
-            if(entry != null && entry.containsKey(key)) {
-                System.out.println("Get stack: {" + key + " : " + entry.get(key) + "}");
-                return entry.get(key);
-            }
-        }
-        value = db.get(key);
+        value = fastSearch.get(key);
         System.out.println("Get non-stack: {" + key + "=>" + value + "}");
         return value;
     }
     //Sets a value in the database. It can happen during a transaction or outside transaction.
     public boolean set(String key, String value) {
-        if(!this.operationsStack.isEmpty()){
-            this.operationsStack.lastElement().put(key, value);
+        fastSearch.put(key, value);
+        if(!operationsStack.isEmpty()){
+            operationsStack.lastElement().put(key, value);
             return true;
         } else {
             db.put(key, value);
--- src/test/java/TestDB.java
+++ src/test/java/TestDB.java
@@ -1,6 +1,9 @@
 import org.example.TransactionalDB;
 import org.junit.Test;
 
+/*
+* de imfrumusetat codul din teste. Vom implementa 4 solutii de transactional db, insa testele raman acelasi pentru toate. Deci testele sunt de 4 ori mai importante decat codul.
+* */
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -8,7 +11,7 @@ import static org.junit.Assert.assertNull;
 public class TestDB {
     TransactionalDB db = new TransactionalDB();
 
-    private void valueCheck(String key, String value) {
+    private void valueCheck(String value) {
         db.set("a", value);           // Operation outside a transaction
         db.set("b", value);           // Operation outside a transaction
         db.set("c", value);           // Operation outside a transaction
@@ -16,14 +19,13 @@ public class TestDB {
         assertEquals("1 added should get "+value, value, db.get("a"));
         assertEquals("1 added should get "+value, value, db.get("b"));
         assertEquals("1 added should get "+value, value, db.get("c"));
-        System.out.println("");
     }
     //for value has to be value
     @Test
     public void testNonTransactionalSet() {
         System.out.println("testNonTransactionalSet");
         assertNull("nothing added should get nothing", db.get("a"));
-        this.valueCheck("", "1");
+        this.valueCheck("1");
     }
 
     //for value has to be value
@@ -31,92 +33,85 @@ public class TestDB {
     public void testTransactionalSet() {
         System.out.println("testTransactionalSet");
         db.begin();                 // Begin a transaction
-        this.valueCheck("", "2");
+        this.valueCheck("2");
     }
 
     @Test
     public void testTransactionInTransactionSet() {
         System.out.println("testTransactionInTransactionSet");
         db.begin();                 // Begin a transaction
-        this.valueCheck("", "2");
+        this.valueCheck("2");
 
         db.begin();
         db.set("c", "3");           // Operation within a nested transaction
-
         assertEquals("2 added should get 2", "2", db.get("a"));
         assertEquals("2 added should get 2", "2", db.get("b"));
         assertEquals("2 added should get 2", "3", db.get("c"));
-        System.out.println("");
     }
 
     @Test
     public void testTransactionInTransactionSetWithCommit() {
         System.out.println("testTransactionInTransactionSetWithCommit");
         db.begin();                 // Begin a transaction
-        this.valueCheck("", "2");
+        this.valueCheck("2");
 
         db.begin();
         db.set("c", "3");           // Operation within a nested transaction
-
         assertEquals("2 added should get 2", "2", db.get("a"));
         assertEquals("2 added should get 2", "2", db.get("b"));
         assertEquals("2 added should get 3", "3", db.get("c"));
-        System.out.println("");
+
         db.commit();
         assertEquals("2 added should get 2", "2", db.get("a"));
         assertEquals("2 added should get 2", "2", db.get("b"));
         assertEquals("2 added should get 3", "3", db.get("c"));
-        System.out.println("");
     }
 
     @Test
     public void testTransactionInTransactionSetWithCommitAndRollback() {
         System.out.println("testTransactionInTransactionSetWithCommitAndRollback");
         db.begin();                 // Begin a transaction
-        this.valueCheck("", "2");
+        this.valueCheck("2");
 
         db.begin();
         db.set("c", "3");           // Operation within a nested transaction
-
         assertEquals("2 added should get 2", "2", db.get("a"));
         assertEquals("2 added should get 2", "2", db.get("b"));
         assertEquals("2 added should get 2", "3", db.get("c"));
-        System.out.println("");
+
         db.commit();
         assertEquals("2 added should get 2", "2", db.get("a"));
         assertEquals("2 added should get 2", "2", db.get("b"));
         assertEquals("2 added should get 2", "3", db.get("c"));
-        System.out.println("");
+
         db.rollback();
         assertNull("2 added should get 2", db.get("a"));
         assertNull("2 added should get 2", db.get("b"));
         assertNull("2 added should get 2", db.get("c"));
-        System.out.println("");
     }
+
     @Test
     public void testTransactionInTransactionSetWithCommitAndRollbackOutsideTransaction() {
         System.out.println("testTransactionInTransactionSetWithCommitAndRollbackOutsideTransaction");
-        this.valueCheck("", "1");
+        this.valueCheck("1");
         db.begin();                 // Begin a transaction
-        this.valueCheck("", "2");
+        this.valueCheck("2");
 
         db.begin();
         db.set("c", "3");           // Operation within a nested transaction
-
         assertEquals("2 added should get 2", "2", db.get("a"));
         assertEquals("2 added should get 2", "2", db.get("b"));
         assertEquals("2 added should get 2", "3", db.get("c"));
-        System.out.println("");
+
         db.commit();
         assertEquals("2 added should get 2", "2", db.get("a"));
         assertEquals("2 added should get 2", "2", db.get("b"));
         assertEquals("2 added should get 2", "3", db.get("c"));
-        System.out.println("");
+
         db.rollback();
         assertEquals("2 added should get 2", "1", db.get("a"));
         assertEquals("2 added should get 2", "1", db.get("b"));
         assertEquals("2 added should get 2", "1", db.get("c"));
-        System.out.println("");
     }
 
     @Test
@@ -125,9 +120,9 @@ public class TestDB {
         db.begin();
         db.set("c", "1");           // Operation within a nested transaction
         assertEquals("1 added should get 1", "1", db.get("c"));
+
         db.begin();
         db.set("c", "3");           // Operation within a nested transaction
-
         assertEquals("3 added should get 3", "3", db.get("c"));
 
         db.commit();
@@ -145,7 +140,6 @@ public class TestDB {
         db.set("c", "1");           // Operation within a nested transaction
         db.begin();
         db.set("c", "3");           // Operation within a nested transaction
-
         assertEquals("3 added should get 3", "3", db.get("c"));
 
         db.rollback();
